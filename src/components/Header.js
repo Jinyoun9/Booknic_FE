c1e6500b779d85d2f04f0f7d60e8e075b301ffde
박진영 <am030813@naver.com>
2024-07-10 23:43:33 +0900

    ADD: 로그인 버튼 추가

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,16 @@
 import React from 'react';
 import '../css/Header.css';
+import {useNavigate} from "react-router-dom";
 
 const Header = () => {
+    const navigate = useNavigate();
+    const loginClickHandle = () =>{
+        navigate("/login");
+    }
     return (
         <header className="header">
-            <div className="logo">NOLDAY</div>
+            <div className="logo">Booknic</div>
+            <button onClick={loginClickHandle}>로그인</button>
             <div className="menu-icon">☰</div>
         </header>
     );
