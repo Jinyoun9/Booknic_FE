066c23617d70ac69ad9946d61b7ddca38aa94778
박진영 <am030813@naver.com>
2024-07-12 15:13:35 +0900

    ADD: 책 정보 모달창 추가

--- src/pages/LibInfoPage.js
+++ src/pages/LibInfoPage.js
@@ -1,49 +1,65 @@
 import React, { useEffect, useState } from "react";
-import {useLocation, useNavigate} from "react-router-dom";
+import { useLocation, useNavigate } from "react-router-dom";
+import Modal from 'react-modal'; // react-modal import 추가
 import Header from "../components/Header";
 import SearchBar from "../components/SearchBar";
 import Footer from "../components/Footer";
 import fetchData from "../fetchData";
 import '../css/LibInfoPage.css';
 import noImage from '../asset/image/noimage.jpg';
+
+Modal.setAppElement('#root'); // 애플리케이션 루트 요소 설정
+
 const LibInfoPage = () => {
     const location = useLocation();
     const navigate = useNavigate();
     const url = new URLSearchParams(location.search);
     const lName = url.get('libName');
-    const {libCode} = location.state || {};
+    const { libCode } = location.state || {};
     const [lCode, setLCode] = useState(null);
     const [libInfo, setLibInfo] = useState([]);
     const [popularBooks, setPopularBooks] = useState([]);
+    const [bookModalIsOpen, setBookModalIsOpen] = useState(false);
+    const [selectedBook, setSelectedBook] = useState(null); // 선택된 책 정보 상태 추가
+
     useEffect(() => {
         if (libCode !== null && libCode !== undefined) {
             setLCode(libCode); // libCode가 유효하면 lCode 상태를 업데이트합니다.
         }
     }, [libCode]);
+
     useEffect(() => {
-        fetchData('api/lib', (response)=>{
-                if(response.length > 0){
-                    const jsonData = response[0];
-                    if(jsonData){
-                        const libs = jsonData.libs.map(item => item.lib);
-                        setLibInfo(libs);
-                    }
+        fetchData('api/lib', (response) => {
+            if (response.length > 0) {
+                const jsonData = response[0];
+                if (jsonData) {
+                    const libs = jsonData.libs.map(item => item.lib);
+                    setLibInfo(libs);
                 }
-            },
-            {libCode});
-        fetchData('api/loanitemlib', (response)=>{
-                if(response.length > 0){
-                    const jsonData = response[0];
-                    if(jsonData){
-                        const docs = jsonData.docs.map(item => item.doc);
-                        setPopularBooks(docs);
-                    }
+            }
+        }, { libCode });
+
+        fetchData('api/loanitemlib', (response) => {
+            if (response.length > 0) {
+                const jsonData = response[0];
+                if (jsonData) {
+                    const docs = jsonData.docs.map(item => item.doc);
+                    setPopularBooks(docs);
                 }
-            },
-            {libCode});
+            }
+        }, { libCode });
     }, [libCode]);
-    console.log(libInfo);
-    console.log(popularBooks);
+
+    const bookModalOpen = (book) => {
+        setSelectedBook(book); // 선택된 책 정보 설정
+        setBookModalIsOpen(true);
+    }
+
+    const bookModalClose = () => {
+        setBookModalIsOpen(false);
+        setSelectedBook(null); // 선택된 책 정보 초기화
+    }
+
     return (
         <div>
             <Header />
@@ -51,7 +67,7 @@ const LibInfoPage = () => {
             <div className="library-info-container">
                 <h2>{lName}</h2>
                 <div>
-                    {libInfo.map((lib, index) =>(
+                    {libInfo.map((lib, index) => (
                         <div key={index} className="lib-info-card">
                             <h3 className="lib-info-title">주소: <span className="lib-info-content">{lib.address}</span></h3>
                             <h3 className="lib-info-title">휴관일: <span className="lib-info-content">{lib.closed}</span></h3>
@@ -65,7 +81,6 @@ const LibInfoPage = () => {
                             )}
                             <h3 className="lib-info-title">전화번호: <span className="lib-info-content">{lib.tel}</span></h3>
                         </div>
-
                     ))}
                     <div className="popular-books-section">
                         <h2>인기 도서</h2>
@@ -74,10 +89,9 @@ const LibInfoPage = () => {
                                 popularBooks.map((book, index) => (
                                     <div key={index} className="popular-book-item">
                                         <img src={book.bookImageURL || noImage} />
-                                        <h3>{book.bookname}</h3>
+                                        <button onClick={() => bookModalOpen(book)}>{book.bookname}</button>
                                         <p>{book.author}</p>
                                         <p>{book.description}</p>
-                                        {/* 추가적인 정보나 버튼 등을 추가할 수 있습니다. */}
                                     </div>
                                 ))
                             ) : (
@@ -85,6 +99,23 @@ const LibInfoPage = () => {
                             )}
                         </div>
                     </div>
+                    <Modal
+                        isOpen={bookModalIsOpen}
+                        onRequestClose={bookModalClose}
+                        contentLabel="Book Modal"
+                        className="book-modal"
+                        overlayClassName="book-modal-overlay"
+                    >
+                        {selectedBook && (
+                            <div className="book-modal-content">
+                                <span className="close-button" onClick={bookModalClose}>&times;</span>
+                                <img src={selectedBook.bookImageURL || noImage} alt={selectedBook.bookname} />
+                                <h2>{selectedBook.bookname}</h2>
+                                <p>저자: {selectedBook.author}</p>
+                                <p>{selectedBook.description}</p>
+                            </div>
+                        )}
+                    </Modal>
                 </div>
             </div>
             <Footer />
