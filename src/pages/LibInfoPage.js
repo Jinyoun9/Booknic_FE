e1a282165ba8b0e6f87122e0ac7d637419b979be
박진영 <am030813@naver.com>
2024-07-28 18:39:23 +0900

    FEAT: 도서 대출 QR코드 생성

--- src/pages/LibInfoPage.js
+++ src/pages/LibInfoPage.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { useLocation, useNavigate } from "react-router-dom";
-import Modal from 'react-modal'; // react-modal import 추가
+import Modal from 'react-modal';
 import Header from "../components/Header";
 import SearchBar from "../components/SearchBar";
 import Footer from "../components/Footer";
@@ -9,6 +9,7 @@ import '../css/LibInfoPage.css';
 import noImage from '../asset/image/noimage.jpg';
 import postData from "../api/postData";
 import {FaStar} from "react-icons/fa";
+import QRCode from 'react-qr-code';
 
 const LibInfoPage = () => {
     const location = useLocation();
@@ -22,9 +23,12 @@ const LibInfoPage = () => {
     const [libInfo, setLibInfo] = useState([]);
     const [popularBooks, setPopularBooks] = useState([]);
     const [bookModalIsOpen, setBookModalIsOpen] = useState(false);
+    const [loanModalIsOpen, setLoanModalIsOpen] = useState(false);
     const [selectedBook, setSelectedBook] = useState(null); // 선택된 책 정보 상태 추가
     const [authToken, setAuthToken] = useState(localStorage.getItem('accessToken') || '');
     const [toggleValue, setToggleValue] = useState(false);
+    const [qrValue, setQrValue] = useState('');
+
     useEffect(() => {
         if (libCode !== null && libCode !== undefined) {
             setLCode(libCode); // libCode가 유효하면 lCode 상태를 업데이트합니다.
@@ -63,6 +67,11 @@ const LibInfoPage = () => {
         setSelectedBook(null); // 선택된 책 정보 초기화
     }
 
+    const loanModalClose = () => {
+        setLoanModalIsOpen(false);
+        setQrValue(null); // 선택된 책 정보 초기화
+    }
+
     const handleFavoriteClick = (bookname, library) => {
         setToggleValue(toggleValue ? 0 : 1);
         const params = {bookname, library, toggleValue};
@@ -73,6 +82,13 @@ const LibInfoPage = () => {
             alert('로그인이 필요한 서비스입니다.');
             navigate('/login');
         }
+    }
+    const handleLoanClick = (bookname, library) => {
+        const accessToken = localStorage.getItem('accessToken');
+        const qrURL = `https://192.168.10.104:8443/book/loan?bookname=${encodeURIComponent(bookname)}&library=${encodeURIComponent(library)}&token=${encodeURIComponent(accessToken)}}`
+        setQrValue(qrURL);
+        setLoanModalIsOpen(true);
+        setBookModalIsOpen(false);
     }
     return (
         <div>
@@ -126,12 +142,25 @@ const LibInfoPage = () => {
                                 <button onClick={() => handleFavoriteClick(selectedBook.bookname, lName)}>
                                     <FaStar size={30} color={toggleValue ? "yellow" : "gray"} />
                                 </button>
+                                <button onClick={() => handleLoanClick(selectedBook.bookname, lName)}>
+                                    대출하기
+                                </button>
                                 <img src={selectedBook.bookImageURL || noImage} alt={selectedBook.bookname} />
                                 <h2>{selectedBook.bookname}</h2>
                                 <p>저자: {selectedBook.author}</p>
                                 <p>{selectedBook.description}</p>
                             </div>
                         )}
+                    </Modal>
+                    <Modal
+                        isOpen = {loanModalIsOpen}
+                        isClose = {loanModalClose}
+                        className = "book-modal"
+                    >
+                        <div style={{ marginTop: '20px' }}>
+                            <span className="close-button" onClick={loanModalClose}>&times;</span>
+                            {qrValue && <QRCode value={qrValue} />}
+                        </div>
                     </Modal>
                 </div>
             </div>
